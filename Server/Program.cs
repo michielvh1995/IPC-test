4e331bf73a7703d3218bb96f23d62e9190b3af82
michielvh1995 <michielvh1995@github.com>
2019-08-24 15:02:02 +0200

    first commit

---
+++ Server/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Diagnostics;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process pipeClient = new Process();
+            pipeClient.StartInfo.FileName = "pipeClient.exe";
+
+            using (AnonymousPipeServerStream pipeServer = new AnonymousPipeServerStream(PipeDirection.Out, HandleInheritability.Inheritable))
+            {
+                Console.WriteLine("[SERVER] Current mode: {0}.", pipeServer.TransmissionMode);
+                // test
+            }
+        }
+    }
+}
